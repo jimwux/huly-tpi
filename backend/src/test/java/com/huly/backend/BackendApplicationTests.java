594861a5207f6ef187362a8d6a1be04c6273205c
MM <143347704+MacielMaximiliano@users.noreply.github.com>
2026-04-25 18:12:07 -0300

    feat:✨initial project setup

---
+++ backend/src/test/java/com/huly/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huly.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
